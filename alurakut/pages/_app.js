b54429294a7ec2245c34d36e03c67e3feda3149c
Flavio Lopes da Silva <digitalblu@gmail.com>
2021-07-15 06:27:34 -0400

    Primeira Aula Imersão Next

---
+++ alurakut/pages/_app.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+
+/* Reset CSS (Necolas Reset CSS <3) */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6
+  }
+
+  #__next {
+    display: flex;
+    min-heigth: 100vh;
+    flex-direction: column;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#red',
+  },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
