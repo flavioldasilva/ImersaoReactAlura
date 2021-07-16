064825fe1207ea0a73b746dce227d97e0b249b9c
Flavio Lopes da Silva <digitalblu@gmail.com>
2021-07-16 02:52:42 -0400

    Primeira aula

--- alurakut/pages/_app.js
+++ alurakut/pages/_app.js
@@ -1,31 +1,36 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-
-/* Reset CSS (Necolas Reset CSS <3) */
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+  /* Reset CSS */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
   body {
     font-family: sans-serif;
-    background-color: #D9E6F6
+    background-color: #D9E6F6;
   }
 
   #__next {
     display: flex;
-    min-heigth: 100vh;
+    min-height: 100vh;
     flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
   }
 `
 
 const theme = {
   colors: {
-    primary: '#red',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -35,5 +40,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -4,29 +4,54 @@ import styled from 'styled-components'
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 ` */
-const Box = styled.div`
-  background: #ffffff;
-  border-radius: 8px
-`;
 
+const Box = styled.div`
+  background: #FFFFFF;
+  border-radius: 8px;
+`
 const MainGrid = styled.main`
-  display: grid;
+  width: 100%;
   grid-gap: 10px;
+  margin-left: auto;
+  margin-rigth:auto;
+  max-width: 500px;
+  padding: 16px;
+  .profileArea {
+    display: none;
+    @media(min-width: 860px) {
+      display: block;
+    }
+  }
+
+  @media(min-width: 860px) {
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea"; 
+    grid-template-columns: 160px 1fr 312px;  
+  }
+  
 `;
 
 export default function Home() {
   return (
-    <main>
-      kkkkkkk
-      <Box>
-        Imagem
-      </Box>
-      <Box>
-        Bem Vindo
-      </Box>
-      <Box>
-        Comunidades
-      </Box>
-    </main>
+    <MainGrid>
+      {/* <Box styled="grid-area: profileArea;"> */}
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <Box>
+          <img src="http://github.com/flavioldasilva.png" />
+        </Box>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem vindo
+        </Box>
+      </div>
+      <div className="profileArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>
+          Comunidades
+        </Box>
+      </div>
+
+    </MainGrid>
   )
 }
